b73dd1427c3c2c2de9c5a3ea835a5374482a6905
Blaze28 <78606303+blaze28@users.noreply.github.com>
2023-04-24 20:07:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -474,14 +474,7 @@ def main():
   #       add_data(blog_author,blog_title,blog_article,blog_post_date)
   #       st.success("Post::'{}' Saved".format(blog_title))
 
-  html3="""
 
-    <div style="color:yellow; margin:80px; text-align:center;">
-      Developed with ❤️ by <a href=https://hrugved06.github.io/Portfolio-Hrugved-Kolhe/> Hrugved Kolhe</a>
-    </div>
-      """
-
-  st.markdown(html3,unsafe_allow_html=True)
 
 if __name__=='__main__':
     main()
